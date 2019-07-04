cf716a60ff82e0812e7b364b063562bc784af4ff
thomaspovinelli <tompov227@gmail.com>
2019-07-03 21:04:57 -0400

    Added messages

--- app.js
+++ app.js
@@ -1,13 +1,36 @@
 var app = require('http').createServer(handler)
 var io = require('socket.io')(app);
 var fs = require('fs');
+const os = require('os');
+const dns = require('dns');
 
-app.listen(8000);
+async function getLocalIP() {
+  return new Promise((resolve, reject) => {
+    dns.lookup(os.hostname(), function (err, add, fam) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(add);
+      }
+    });
+  })
+}
+
+
+const PORT = 8000;
+app.listen(PORT);
+getLocalIP().then((address) => {
+  console.log(`[+] Hosting server on ${address}:${PORT}`);
+}).catch((err) => {
+  console.error(err);
+  throw err;
+});
 
 function handler(req, res) {
-  let p;
-  if (req.url == '/') { p = __dirname + '/index.html'; }
-  else { p = __dirname + req.url; }
+  let p = __dirname + req.url;
+  if (req.url == '/') {
+    p += 'index.html';
+  }
   fs.readFile(p, function (err, data) {
     res.end(data);
   });
@@ -17,6 +40,7 @@ function handler(req, res) {
 let id = 1;
 
 io.on('connection', function (socket) {
+  console.log("[+] Connecting to client at " + socket.handshake.address);
   socket.emit('connected', { id: id++ });
   socket.on('mouse pressed event', function (data) {
     socket.emit('ack', { success: true });
@@ -26,7 +50,13 @@ io.on('connection', function (socket) {
     io.emit('mouse released', {});
   });
   socket.on('clear canvas', function (data) {
-    console.log('socket said clear; io sending clear');
     io.emit('clear canvas', {});
   });
+  socket.on('disconnect', function (data) {
+    console.log('[-] Client disconnecting ' + socket.handshake.address);
+  })
+});
+
+io.on('disconnect', function (socket) {
+  console.log('[-] Disconnected from client ' + socket.handleshake.address);
 });
